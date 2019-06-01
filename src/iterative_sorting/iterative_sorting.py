d35df2c89d350c64f6934bbe5d1173122f54bdfb
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-01 13:55:47 -0700

    Add comments to explain bubble_sort algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -16,7 +16,18 @@ def selection_sort( arr ):
 
 
 """
-
+bubble_sort takes in an array.
+1) We initialize a boolean flag called swapped to be True
+2) While swapped is True keep looping:
+    *) First set swapped to false inside the while loop
+    *) Then run a for loop, starting at 0 and going through the end of the array.
+        >) Inside the for loop check to see if the left index is larger than the right index
+            if it is swap, then set swapped to True
+        >) Continue through for loop until end.
+    *) Check if swapped == False, because if we didn't go into the if statement inside of the for loop
+then that means no items swapped, which means the entire array is sorted from lowest to highest,
+which means that swapped will == False, and the while loop will break
+3) return the sorted version of the array.
 """
 
 
