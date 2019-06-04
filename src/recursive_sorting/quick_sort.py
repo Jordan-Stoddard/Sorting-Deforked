9a4cf50dc7f0fb881e3fba291844510bc619dc9d
Jordan Stoddard <jjsincorporated@gmail.com>
2019-06-04 15:38:58 -0700

    comment out in place quick_sort

--- src/recursive_sorting/quick_sort.py
+++ src/recursive_sorting/quick_sort.py
@@ -1,8 +1,8 @@
 int_list = [5, 9, 3, 7, 2, 8, 1, 6]
 
-def partition(list):
+def partition(l):
     left = []
-    pivot = list[0]
+    pivot = l[0]
     right = []
     
     for v in l[1:]:
@@ -13,42 +13,44 @@ def partition(list):
 
     return left, pivot, right
 
-quick_sort(l):
+def quick_sort(l):
     if len(l) <= 1:
         return l
 
     left, pivot, right = partition(l)
 
-    return quicksort(left) + [pivot] + quicksort(right)
+    return quick_sort(left) + [pivot] + quick_sort(right)
 
-# In place quick_sort
-    def quick_sort_A( books, low, high ):
-    # base case
-    if low >= high:
-        return books
-    # recursive case
-    else:
-        # divide
-        pivot_index = low
-        # for each element in subarray
-        for i in range(low, high):
-            if books[i].genre < books[pivot_index].genre:
-                # double swap to move smaller elements to correct index
-                # move current element to the right of pivot
-                temp = books[pivot_index+1]
-                books[pivot_index+1] = books[i]
-                books[i] = temp
+print(quick_sort(int_list))
 
-                # swap pivot with element on its right
-                temp = books[pivot_index]
-                books[pivot_index] = books[pivot_index+1]
-                books[pivot_index+1] = temp
-                pivot_index += 1
+# # In place quick_sort
+#     def quick_sort_A( books, low, high ):
+#     # base case
+#     if low >= high:
+#         return books
+#     # recursive case
+#     else:
+#         # divide
+#         pivot_index = low
+#         # for each element in subarray
+#         for i in range(low, high):
+#             if books[i].genre < books[pivot_index].genre:
+#                 # double swap to move smaller elements to correct index
+#                 # move current element to the right of pivot
+#                 temp = books[pivot_index+1]
+#                 books[pivot_index+1] = books[i]
+#                 books[i] = temp
 
-        # conquer
-        # Quick Sort everything left of the pivot
-        books = quick_sort_A(books, low, pivot_index)
-        # Quick Sort everything right of the pivot
-        books = quick_sort_A(books, pivot_index+1, high)
+#                 # swap pivot with element on its right
+#                 temp = books[pivot_index]
+#                 books[pivot_index] = books[pivot_index+1]
+#                 books[pivot_index+1] = temp
+#                 pivot_index += 1
+
+#         # conquer
+#         # Quick Sort everything left of the pivot
+#         books = quick_sort_A(books, low, pivot_index)
+#         # Quick Sort everything right of the pivot
+#         books = quick_sort_A(books, pivot_index+1, high)
   
-        return books
+#         return books
